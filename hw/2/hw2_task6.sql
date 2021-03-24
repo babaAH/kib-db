31a9bf5d3aaf58b6d364ff57aae4bacb071c8918
babaAH <shama_chich@icloud.com>
2021-03-24 08:30:33 +0300

    сделана Первая часть задания №6

--- hw/2/hw2_task6.sql
+++ hw/2/hw2_task6.sql
@@ -1,13 +1,8 @@
 
-select last_name from customer
+/*
+    Сегментация клиентов по количеству букв `а` в его фамилии
 
-select (a_in_last_name) as cnt, count(a_in_last_name)  from (
-    select last_name,
-           length(last_name) - length(replace(last_name, 'a', '')) as a_in_last_name
-    from customer
-) as t
-group by cnt
-
+*/
 select case when cnt=0 then 'A'
             when cnt=1 then 'B'
             when cnt=2 then 'C'
--- hw/2/hw2_task7.sql
+++ hw/2/hw2_task7.sql
@@ -10,3 +10,120 @@ SELECT genre_id, max(cnt) FROM (
 
 GROUP BY  genre_id
 order by genre_id
+
+
+
+
+SELECT * FROM customer WHERE company IS NOT NULL
+
+
+SELECT * FROM customer WHERE company IS NOT NULL
+
+
+
+SELECT g.name, count(DISTINCT track.track_id) FROM track
+    LEFT JOIN genre g on g.genre_id = track.genre_id
+GROUP BY g.genre_id
+
+
+SELECT * FROM (SELECT g.name, count(DISTINCT track.track_id) FROM track
+    LEFT JOIN genre g on g.genre_id = track.genre_id
+    LEFT JOIN invoice_line inv_line on inv_line.track_id = track.track_id
+GROUP BY g.genre_id)
+
+
+SELECT c_name , g_id, max(g_count)  FROM (
+    SELECT company as c_name, t.genre_id as g_id, count(t.genre_id) as g_count
+        FROM customer
+            LEFT JOIN invoice i on customer.customer_id = i.customer_id
+            LEFT JOIN invoice_line il on i.invoice_id = il.invoice_id
+            LEFT JOIN track t on t.track_id = il.track_id
+        WHERE customer.company IS NOT NULL
+
+        GROUP BY company
+        ORDER BY company
+    ) as company_genres_count
+GROUP BY c_name, g_id
+ORDER BY c_name
+
+
+
+
+
+-- Количество приобретенных композиций по жанрам для каждой отдельно взятой компании
+SELECT customer.customer_id as c_id, company as company_name, genre_id, count(t.genre_id) as cnt FROM customer
+    left join invoice i on customer.customer_id = i.customer_id
+    left join invoice_line il on i.invoice_id = il.invoice_id
+    left join track t on t.track_id = il.track_id
+where company is not null
+GROUP BY c_id, company, genre_id
+order by c_id
+
+
+
+
+
+SELECT company_name, genre_id, max(cnt) mx FROM (
+    SELECT customer.customer_id as c_id, company as company_name, genre_id, count(t.genre_id) as cnt FROM customer
+        left join invoice i on customer.customer_id = i.customer_id
+        left join invoice_line il on i.invoice_id = il.invoice_id
+        left join track t on t.track_id = il.track_id
+    where company is not null
+    GROUP BY c_id, company, genre_id
+    order by c_id
+) as company_genres_count
+
+GROUP BY  genre_id, company_name
+order by genre_id
+
+SELECT max(mx) FROM (
+
+    SELECT company_name, genre_id, max(cnt) mx FROM (
+        SELECT customer.customer_id as c_id, company as company_name, genre_id, count(t.genre_id) as cnt FROM customer
+            left join invoice i on customer.customer_id = i.customer_id
+            left join invoice_line il on i.invoice_id = il.invoice_id
+            left join track t on t.track_id = il.track_id
+        where company is not null
+        GROUP BY c_id, company, genre_id
+        order by c_id
+    ) as company_genres_count
+
+    GROUP BY  genre_id, company_name
+    order by genre_id
+) as comp_genre_cnt
+
+
+select  max(mx) from (
+              SELECT company_name, genre_id, max(cnt) mx FROM (
+    SELECT customer.customer_id as c_id, company as company_name, genre_id, count(t.genre_id) as cnt FROM customer
+        left join invoice i on customer.customer_id = i.customer_id
+        left join invoice_line il on i.invoice_id = il.invoice_id
+        left join track t on t.track_id = il.track_id
+    where company is not null
+    GROUP BY c_id, company, genre_id
+    order by c_id
+) as company_genres_count
+
+GROUP BY  genre_id, company_name
+order by genre_id
+                  ) as company_genres_count
+where genre_id=1
+
+
+
+select  max(mx) from (
+              SELECT genre_id, array_agg((cnt)) mx FROM (
+    SELECT customer.customer_id as c_id, company as company_name, genre_id, count(t.genre_id) as cnt FROM customer
+        left join invoice i on customer.customer_id = i.customer_id
+        left join invoice_line il on i.invoice_id = il.invoice_id
+        left join track t on t.track_id = il.track_id
+    where company is not null
+    GROUP BY c_id, company, genre_id
+    order by c_id
+) as company_genres_count
+
+GROUP BY  genre_id
+order by genre_id
+                  ) as company_genres_count
+where genre_id=1
+
